3de5c3d339f5bc742d9799155882ff84f6c6bd1f
gooooloo <qidu.lin@gmail.com>
2013-03-31 20:15:48 +0800

    add TODO mark

--- src/com/qidu/lin/showRecentApps/RecentAppsLayoutOperater.java
+++ src/com/qidu/lin/showRecentApps/RecentAppsLayoutOperater.java
@@ -71,6 +71,7 @@ class RecentAppsLayoutOperater implements LayoutOperator
 	{
 		for (int i = 0; i < count; i++)
 		{
+			// TODO : inflate takes too long time.
 			View view = fi.inflate(R.layout.entry, null);
 			viewInfos.put(view, new Pair<Boolean, Integer>(true, viewInfos.size()));
 
